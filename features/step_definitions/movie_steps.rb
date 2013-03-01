2c022cfd8d5f3fd215c79dba595fc1ee86ffedea
Daniel Swiecki <dswiecki@colgate.edu>
2013-03-01 18:12:08 -0500

    finished hw 3

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,25 +4,27 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
-    Movie.new(:title => movie[:title], :rating => movie[:rating], :description => movie[:description], :release_date => movie[:release_date])
+    Movie.create(:title => movie[:title], :rating => movie[:rating], :description => movie[:description], :release_date => movie[:release_date])
   end
 end
 
-# Make sure that one string (regexp) occurs before or after another one
-#   on the same page
-
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
-  #  ensure that that e1 occurs before e2.
-  #  page.body is the entire content of the page as a string.
-  flunk "Unimplemented"
+  # still at 99 problems since we didn't use a regex for this
+  page.body.index(e1).should < page.body.index(e2)
 end
 
-# Make it easier to express checking or unchecking several boxes at once
-#  "When I uncheck the following ratings: PG, G, R"
-#  "When I check the following ratings: G"
-
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
-  # HINT: use String#split to split up the rating_list, then
-  #   iterate over the ratings and reuse the "When I check..." or
-  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list.split.each do |rating|
+    rating.gsub!(/,/, '')
+    if uncheck.to_s == ''
+      step %Q{I check "ratings[#{rating}]"}
+    else
+      step %Q{I uncheck "ratings[#{rating}]"}
+    end
+  end
+end
+
+Then /I should see all of the movies/ do
+  table = page.all("#movies tr").size
+  Movie.count.should == table -1 
 end
